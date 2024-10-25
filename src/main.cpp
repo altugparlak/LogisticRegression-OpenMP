1ce22a3db9a02df9be3199002424e82e2e84a8e3
Altug Parlak <alparlak1@gmail.com>
2024-10-25 17:05:52 +0300

    opencv setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <omp.h>
+#include <opencv2/opencv.hpp>
 #include "utils.h"
 
 int main(int argc, char** argv) {
@@ -15,6 +16,16 @@ int main(int argc, char** argv) {
         double value = 0.5;
         double sig = sigmoid(value);
         printf("Sigmoid of %lf is %lf\n", value, sig);
+
+        std::string image_path = "../dataset/Train/dog.1.jpg";
+
+        cv::Mat image = cv::imread(image_path, cv::IMREAD_COLOR);
+        if(image.empty()) {
+            std::cerr << "Error: Could not open or find the image!" << std::endl;
+        }
+
+        cv::imshow("Loaded Image", image);
+        cv::waitKey(0);
     }
 
     return 0;
